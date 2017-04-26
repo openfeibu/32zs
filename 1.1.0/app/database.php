3136e5cb443058606d72fb60e521384e927c5ba3
wzj <wzj@497c1e10-dea7-eb4f-bd38-8d20dcda2260>
2017-04-26 09:23:46 +0000

    git-svn-id: https://39.108.144.9:8443/svn/32zs@6 497c1e10-dea7-eb4f-bd38-8d20dcda2260

--- 1.1.0/app/database.php
+++ 1.1.0/app/database.php
@@ -12,14 +12,24 @@
 return [
     // 数据库类型
     'type'           => 'mysql',
-    // 服务器地址
+/*
     'hostname'       => '127.0.0.1',
     // 数据库名
     'database'       => 'zhaosheng',
     // 用户名
     'username'       => 'root',
     // 密码
-    'password'       => '5PALtJKE1YuijpKS',
+    'password'       => 'root',
+    */
+    // 服务器地址
+    'hostname'       => '211.66.88.153',
+    // 数据库名
+    'database'       => 'zhaosheng',
+    // 用户名
+    'username'       => 'feibukeji',
+    // 密码
+    'password'       => 'bgyrtksithv,1*&($AC',
+
     // 端口
     'hostport'       => '3306',
     // 连接dsn
