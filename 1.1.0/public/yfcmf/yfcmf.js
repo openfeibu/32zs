3c3f3feef0e4ab5025a26d483fc90e06e7612b9f
wzjxh <wzjxh@497c1e10-dea7-eb4f-bd38-8d20dcda2260>
2017-05-18 16:55:57 +0000

    git-svn-id: https://39.108.144.9:8443/svn/32zs@164 497c1e10-dea7-eb4f-bd38-8d20dcda2260

--- 1.1.0/public/yfcmf/yfcmf.js
+++ 1.1.0/public/yfcmf/yfcmf.js
@@ -1279,7 +1279,7 @@ $(function(){
 				success: function(data){
 					$('#loading_img').remove();
 					$this.after('<span class="save_span">已保存</span>');
-					var html = '<span style="color:#3499DB;font-weight: bold;">待审核</span>';
+					var html = '<span style="color:#3499DB;font-weight: bold;">审核中</span>';
 					$(".status_" + member_list_id).html(html);
 					$('.save_span').fadeOut('normal').remove();
 				}
