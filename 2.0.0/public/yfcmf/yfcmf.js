16887d37a1611f8a8bcb68a14c89c6e2b12ad712
yjc <yjc@497c1e10-dea7-eb4f-bd38-8d20dcda2260>
2018-04-25 09:13:06 +0000

    git-svn-id: https://39.108.144.9:8443/svn/32zs@262 497c1e10-dea7-eb4f-bd38-8d20dcda2260

--- 2.0.0/public/yfcmf/yfcmf.js
+++ 2.0.0/public/yfcmf/yfcmf.js
@@ -965,6 +965,7 @@ $(function () {
 	$('body').on('change','.ajax_change',function () {
 		load = layer.load(2);
         var $form = $(this).parents("form");
+
 		$.ajax({
 		    url:$form.attr('action'),
 			type:"POST",
@@ -972,6 +973,7 @@ $(function () {
 			success: function(data,status){
 				if(typeof load!="undefined"){layer.close(load);}
 				$("#ajax-data").html(data);
+                window.location.href="#/?activetype_check=1&school_id=&major_id=&key="
 			}
 		});
     });
