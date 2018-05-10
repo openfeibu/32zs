ebe6b1b99218994080c1eb2296062c44e93e2eb8
wzj <wzj@497c1e10-dea7-eb4f-bd38-8d20dcda2260>
2018-05-10 12:26:47 +0000

    git-svn-id: https://39.108.144.9:8443/svn/32zs@371 497c1e10-dea7-eb4f-bd38-8d20dcda2260

--- 2.0.0/app/common.php
+++ 2.0.0/app/common.php
@@ -1665,7 +1665,6 @@ function get_birth($idcard)
 function get_sex($idcard)
 {
     return substr($idcard, -2, 1) % 2 ? '男' : '女';
-    //return substr($idcard, (strlen($idcard)==15 ? -2 : -1), 1) % 2 ? '男' : '女';
 }
 function is_idcard($id)
 {
