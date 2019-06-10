65c09828af0bef48d210584ddb0c555d964d7a02
wzj <wzj@497c1e10-dea7-eb4f-bd38-8d20dcda2260>
2019-06-10 07:35:25 +0000

    git-svn-id: https://39.108.144.9:8443/svn/32zs@555 497c1e10-dea7-eb4f-bd38-8d20dcda2260

---
+++ 2.0.0/statistic.php
@@ -0,0 +1,3 @@
+<?php
+    $info = file_get_contents("http://32zs.gdngs.cn/home/statistics/index.html");
+?>
---
+++ 2.0.0/update.bat
@@ -0,0 +1,5 @@
+@echo off
+
+"E:\UPUPW_K2.1_64\PHPX\PHP70\php.exe"  -c  .  E:\UPUPW_K2.1_64\htdocs\32zs\2.0.0\statistic.php
+
+exit
