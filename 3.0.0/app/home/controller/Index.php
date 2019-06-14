08658fa6ad45818780e999a71aa74b83be016f9e
wzj <wzj@497c1e10-dea7-eb4f-bd38-8d20dcda2260>
2019-06-14 07:44:15 +0000

    git-svn-id: https://39.108.144.9:8443/svn/32zs@576 497c1e10-dea7-eb4f-bd38-8d20dcda2260

--- 3.0.0/app/home/controller/Index.php
+++ 3.0.0/app/home/controller/Index.php
@@ -11,6 +11,9 @@ namespace app\home\controller;
 use think\Cache;
 use think\Db;
 use think\captcha\Captcha;
+use app\services\Certificate;
+use Mohuishou\ImageOCR\Image;
+use Mohuishou\ImageOCR\StorageFile;
 
 class Index extends Base
 {
@@ -87,4 +90,96 @@ class Index extends Base
 			}
 		}
 	}
+	public function test()
+    {
+        $certificate =  new Certificate();
+/*
+        $member_list_username = '44528119970825031X';
+        $member_list_nickname = '许钊纹';
+        $exam_number = '528116102100002';
+        $year = '2016';
+        $exam_info = [
+            'member_list_id' => 1,
+            'member_list_username' => $member_list_username,
+            'member_list_nickname' => $member_list_nickname,
+            'exam_number' => $exam_number,
+            'year' => $year,
+            'exam_date' => '2016_a'
+        ];
+        return $certificate->PSE($exam_info);
+*/
+
+        $image_name = 'verify_img'.time().'.jpg';
+        $folder = 'verify_images';
+        $img_url = "http://search.neea.edu.cn/Imgs.do?act=verify&t=0.8841180045674784";
+        $header[] = "Referer: http://search.neea.edu.cn/QueryMarkUpAction.do?act=doQueryCond&sid=300&pram=certi";
+        $header[] = "Accept: image/webp,image/apng,image/*,*/*;q=0.8";
+        $header[] = "Accept-Encoding: gzip, deflate";
+        $header[] = "Accept-Language: zh-CN,zh;q=0.9,en;q=0.8";
+        $header[] = "Connection: keep-alive";
+        $header[] = "Host: search.neea.edu.cn";
+        $header[] = "Referer: http://search.neea.edu.cn/QueryMarkUpAction.do?act=doQueryCond&sid=300&pram=certi";
+        $header[] = "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36";
+        $save_image = save_image($img_url,$folder,$image_name,$header);
+        $image_path = $save_image['save_path'];
+        $send = input('send','');
+
+        if($send)
+        {
+            $image = new Image(input('image_path'));
+            $code = input('code');
+            $code_arr = str_split($code);
+            $db = new StorageFile();
+
+            for($i=0;$i< $image::CHAR_NUM;$i++){
+                $hash_img_data=implode("",$image->splitImage($i));
+                $db->add($code_arr[$i],$hash_img_data);
+            }
+        }
+
+        $verify_image = new Image($image_path);
+        $a=$verify_image->find();
+        $verify_code = implode("",$a);
+
+        $this->assign('verify_code',$verify_code);
+        $this->assign('image_path',$image_path);
+        $this->assign('image_url','/data/upload/verify_images/'.$image_name);
+
+        return $this->view->fetch(':test');
+        /*
+
+
+        $image_name = 'verify_img'.time().'.jpg';
+        $folder = 'verify_images';
+        $img_url = "http://query.5184.com/query/image.jsp?time=".math_random();
+        $save_image = save_image($img_url,$folder,$image_name);
+        $image_path = $save_image['save_path'];
+
+        $send = input('send','');
+
+        if($send)
+        {
+            $image = new Image(input('image_path'));
+            $code = input('code');
+            $code_arr = str_split($code);
+            $db = new StorageFile();
+
+            for($i=0;$i< $image::CHAR_NUM;$i++){
+                $hash_img_data=implode("",$image->splitImage($i));
+                $db->add($code_arr[$i],$hash_img_data);
+            }
+        }
+
+        $verify_image = new Image($image_path);
+        $a=$verify_image->find();
+        $verify_code = implode("",$a);
+
+        $this->assign('verify_code',$verify_code);
+        $this->assign('image_path',$image_path);
+        $this->assign('image_url','/data/upload/verify_images/'.$image_name);
+
+        return $this->view->fetch(':test');
+       // return $certificate->test();
+        */
+    }
 }
