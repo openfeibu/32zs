156aaa1b3c8f621094621b778521bb9907d17322
wzj <wzj@497c1e10-dea7-eb4f-bd38-8d20dcda2260>
2019-09-04 03:17:45 +0000

    git-svn-id: https://39.108.144.9:8443/svn/32zs@585 497c1e10-dea7-eb4f-bd38-8d20dcda2260

--- 3.0.0/public/yfcmf/yfcmf.js
+++ 3.0.0/public/yfcmf/yfcmf.js
@@ -1497,7 +1497,7 @@ $(function(){
 		var reg = /^[0-9]+.?[0-9]*$/;
 		$this = $(this);
 		value = $this.val();
-        if(!reg.test(value) && !empty(value)){
+        if(!reg.test(value) && value.length != 0){
 			$this.after('<span class="save_span">格式不正确</span>');
 			$('.save_span').fadeOut('normal').remove();
 			$this.val('');
